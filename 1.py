539c4b796c7ec0a8575b7b9b195ec37b4bbe762a
shubham37204 <shubhambhardwaj37204@gmail.com>
2025-03-02 16:18:08 +0530

    hey

---
+++ 1.py
@@ -0,0 +1,15 @@
+class MyClass:
+    memebrship = True
+    def __init__(self,name, age):
+        self.name= name
+        self.age = age
+
+    def MyFun(self):
+        print(f'{MyClass.memebrship}') #true
+        print(f'{self.memebrship}')    #true
+        print(f'{self.name}')          #shubham
+        print('done')
+
+MyName = MyClass('shubham',25)
+print(MyName.name) 
+print(MyName.MyFun())  #since the function does not return anything hence after printing every print statement it give a none as a output 
